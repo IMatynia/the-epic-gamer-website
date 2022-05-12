ff1eef25900c8f18fe43a7a73d082daff38392a6
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-05-12 13:24:35 +0200

    temp no database

--- app/models/articles.php
+++ app/models/articles.php
@@ -3,24 +3,25 @@ class Articles
 {
     public function __construct()
     {
-        $this->filename = $_SERVER['DOCUMENT_ROOT'] . "/database/articles.json";
-        $raw_database = file_get_contents($this->filename);
-        $this->all_articles = json_decode($raw_database);
+        $this->filename = APPROOT . "/database/articles.json";
+        // $raw_database = file_get_contents($this->filename);
+        //$this->all_articles = json_decode($raw_database);
     }
 
     public function getArticleByIdentifier($identifier)
     {
-        foreach ($this->all_articles as $article) {
-            if($article->id == $identifier)
-            {
-                return $article;
-            }
-        }
-        die("Article " . $identifier . " was not found!");
+        // foreach ($this->all_articles as $article) {
+        //     if($article->id == $identifier)
+        //     {
+        //         return $article;
+        //     }
+        // }
+        // die("Article " . $identifier . " was not found!");
     }
 
     public function getAllArticles()
     {
-        return $this->all_articles;
+        // return $this->all_articles;
+        return [];
     }
 }
