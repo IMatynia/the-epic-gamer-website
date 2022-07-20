bbd5cce980744e9e73797d2c5d7f63d4f5ef2a27
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-07-20 12:04:12 +0200

    removed database class, ill use json instead

--- app/require.php
+++ app/require.php
@@ -2,11 +2,8 @@
     //Require libraries from folder libraries
     require_once 'libraries/Core.php';
     require_once 'libraries/Controller.php';
-    require_once 'libraries/Database.php';
 
     require_once 'config/config.php';
-
-    require_once 'views/includes/head.php';
 
     //Instantiate core class
     $init = new Core();
