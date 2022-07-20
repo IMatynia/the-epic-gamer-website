f1eb4862433e66580a12a47ad1039d31162e2dc9
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-07-20 12:13:02 +0200

    cleared up config

--- app/config/config.php
+++ app/config/config.php
@@ -1,31 +1,13 @@
 <?php
-//APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));
+define('ARTICLES_DB_FILE', APPROOT . "/database/articles.json");
+define('TAGS_DB_FILE', APPROOT . "/database/tags.json");
 
-if(APPROOT == "D:\\Programming\\the-epic-gamer-website\\app")
-{
-    define('DB_HOST', 'eu-cdbr-west-02.cleardb.net'); //Add your db host
-    define('DB_USER', 'bf6d0ebec5a2f9'); // Add your DB root
-    define('DB_PASS', '1c957d41'); //Add your DB pass
-    define('DB_NAME', 'heroku_5c1b88cec025cd7'); //Add your DB Name
-
+if (APPROOT == "D:\\Programming\\the-epic-gamer-website\\app") {
     define('URLROOT', 'http://localhost/');
-}
-else
-{
-    // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    // $cleardb_server = $cleardb_url["host"];
-    // $cleardb_username = $cleardb_url["user"];
-    // $cleardb_password = $cleardb_url["pass"];
-    // $cleardb_db = substr($cleardb_url["path"], 1);
-    
-    // // Database params
-    // define('DB_HOST', $cleardb_server); //Add your db host
-    // define('DB_USER', $cleardb_username); // Add your DB root
-    // define('DB_PASS', $cleardb_password); //Add your DB pass
-    // define('DB_NAME', $cleardb_db); //Add your DB Name
-
+} else {
     define('URLROOT', 'https://the-epic-gamer.herokuapp.com/');
 }
+
 //Sitename
-define('SITENAME', 'The epic gamer');
+define('SITENAME', 'The Epic Gamer');
