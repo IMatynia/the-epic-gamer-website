1733ef9987da752e1f534bdf5fe00a8534308010
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-08-22 10:50:33 +0200

    database lib

--- app/require.php
+++ app/require.php
@@ -4,6 +4,7 @@
     require_once 'libraries/Controller.php';
     require_once 'libraries/OGPdata.php';
     require_once 'libraries/Utilities.php';
+    require_once 'libraries/Database.php';
 
     require_once 'config/config.php';
 
