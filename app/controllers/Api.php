f6e024ab1ceddb453748ff80a56bbad398dd0a9f
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-08-22 12:30:10 +0200

    api checks for api key

--- app/controllers/Api.php
+++ app/controllers/Api.php
@@ -5,10 +5,38 @@ class Api extends Controller
     {
         $this->tags = $this->model("tags");
         $this->articles = $this->model("articles");
+        $this->api_keys = $this->model("api_keys");
+
+        if(isset($_GET["api_key"]))
+        {
+            $current_api_key = $_GET["api_key"];
+        }
+        else if(isset($_POST["api_key"]))
+        {
+            $current_api_key = $_POST["api_key"];
+        }
+        else
+        {
+            die("Api key missing! add api_key as an argument to your request");
+        }
+        
+        $this->permission_granted = $this->api_keys->checkPrivilige($current_api_key);
+        if (!$this->permission_granted) {
+            die("Your api key ran out of requests!");
+        }
     }
 
     public function getAllTags()
     {
         $this->view("json_display", $this->tags->getAllTags());
+    }
+    
+    public function addTag()
+    {
+        $name = $_GET["name"];
+        $description = $_GET["description"];
+
+        $this->tags->addTag($name, $description);
+        die("Tag ".$name." added!");
     }
 }
