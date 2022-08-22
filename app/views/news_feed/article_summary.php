ebe6473ab94ddd1ee07c43b0b63924ee82e2c820
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-08-22 13:56:49 +0200

    identifier is in the article description

--- app/views/news_feed/article_summary.php
+++ app/views/news_feed/article_summary.php
@@ -11,7 +11,7 @@
     </div>
 
     <div class="text_summary">
-        <h1><a href="<?php echo URLROOT . "news/show/" . $identifier; ?>"><?php echo $article->title; ?></a></h1>
+        <h1><a href="<?php echo URLROOT . "news/show/" . $article->identifier; ?>"><?php echo $article->title; ?></a></h1>
         <h2> <?php echo $article->date_published . " | " . $article->author . " | " . join(" / ", $article->tags); ?>
         </h2>
         <p> <?php echo $article->text_summary; ?> </p>
