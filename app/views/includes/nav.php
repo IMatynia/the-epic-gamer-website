8073bc24359b13da69e4f06aafda5bc560d59ce8
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-08-23 02:10:28 +0200

    delicate shadow at nav

--- app/views/includes/nav.php
+++ app/views/includes/nav.php
@@ -3,10 +3,10 @@
         <a href="<?php echo URLROOT?>">THE EPIC GAMER</a></div>
     </div>
     <div class="link_bar">
-        <a class="cool_hover" href="<?php echo URLROOT . "news"; ?>"><div>NEWS</div></a>
-        <a class="cool_hover" href="<?php echo URLROOT . "trollfacepl"; ?>"><div>FREE MONEY GLITCH</div></a>
-        <a class="cool_hover" href="<?php echo URLROOT . "quiz"; ?>"><div>QUIZES</div></a>
-        <a class="cool_hover" href="<?php echo URLROOT . "contact"; ?>"><div>CONTACT</div></a>
-        <a class="cool_hover" href="<?php echo URLROOT . "about"; ?>"><div>ABOUT</div></a>
+        <a class="cool_hover delicate_shadow" href="<?php echo URLROOT . "news"; ?>"><div>NEWS</div></a>
+        <a class="cool_hover delicate_shadow" href="<?php echo URLROOT . "trollfacepl"; ?>"><div>FREE MONEY GLITCH</div></a>
+        <a class="cool_hover delicate_shadow" href="<?php echo URLROOT . "quiz"; ?>"><div>QUIZES</div></a>
+        <a class="cool_hover delicate_shadow" href="<?php echo URLROOT . "contact"; ?>"><div>CONTACT</div></a>
+        <a class="cool_hover delicate_shadow" href="<?php echo URLROOT . "about"; ?>"><div>ABOUT</div></a>
 </div>
 </nav>
