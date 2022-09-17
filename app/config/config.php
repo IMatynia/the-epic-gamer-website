4c202a320b82aa355f0e902f71dfb38bff675436
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-09-17 17:30:54 +0200

    url root now works on local networks

--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,9 @@
 <?php
 define('APPROOT', dirname(dirname(__FILE__)));
-define('ARTICLES_DB_FILE', APPROOT . "/database/articles.json");
-define('TAGS_DB_FILE', APPROOT . "/database/tags.json");
 define('DEFAULT_SITE_IMAGE', APPROOT . "public/media/reklamy/ez money.webp");
 
 if (APPROOT == "D:\\Programming\\the-epic-gamer-website\\app") {
-    define('URLROOT', 'http://localhost/');
+    define('URLROOT', "http://" . $_SERVER["SERVER_NAME"] . "/");
 
     $cleardb_server = 'localhost:3306';
     $cleardb_username = 'root';
