b190dfebb5a02c0a7252f9cd1a5da9dec1ddee46
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-09-17 17:35:15 +0200

    improved footer

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,3 +1,3 @@
 <footer>
-    <div>Matyniov's Corp. | No rights reserved</div>
+    <div>Matyniov's Corp. | Most right reserved, i think? I mean, the website is my idea, the code was written by me, the contents of the articles and quizes are also bright examples of my imagination, but the images are mostly links to external sources for the sake of server space saving. I think thats not that big of a deal, right?</div>
 </footer>
