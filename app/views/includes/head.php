2aedb5e347f269e6ff70fdca4350f56c45b6c273
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-09-17 17:30:07 +0200

    added favicon

--- app/views/includes/head.php
+++ app/views/includes/head.php
@@ -8,17 +8,19 @@
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="<?php echo URLROOT ?>/public/js/hax_script.js"></script>
+
+    <link rel="icon" type="image/x-icon" href="<?php echo URLROOT ?>/public/media/favicon.ico">
+
     <?php
     if (isset($data["ogp_data"])) {
         require_once APPROOT . '/views/includes/ogp_header.php';
-        $final_title = $data["ogp_data"]->title;
+        $final_title = $data["ogp_data"]->title . " - " . SITENAME;
     } else {
         $final_title = SITENAME;
     }
 
     if (isset($data["page_title"])) {
-        $final_title = $data["page_title"];
+        $final_title = $data["page_title"] . " - " . SITENAME;
     }
     echo '<title>' . $final_title . '</title>';
     ?>
