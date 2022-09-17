1942194a5a051bde4d005b5608f7333549e0b8fa
IMatynia <45515142+IMatynia@users.noreply.github.com>
2022-09-17 17:35:37 +0200

    implemented about and contact sections

--- app/views/misc/about_index.php
+++ app/views/misc/about_index.php
@@ -3,13 +3,25 @@ require_once APPROOT . '/views/includes/head.php';
 require_once APPROOT . '/views/includes/nav.php';
 ?>
 
-<div class="sections">
-    <div class="section">
-        <h1>
-            Welcome to the real news for epic gamers
-        </h1>
-        <h3>
-            We are here to provite trust worthy content for you.
-        </h3>
-    </div>
+<head>
+    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/nice_default_tags.css">
+    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/misc.css">
+</head>
+
+<div class="sections nice_default_tags">
+    <div class="inspiration">Welcome to the only unbiased, uncensored news source for pogger gamers like you!</div>
+    <h1 id="mission">Our mission</h1>
+    <h3>Gamers in our society are arguably the most opressed minorty out there. Everywhere in media we are portrayed as fat, ugly, lonely basement dwelers. This destructive image
+        creates social tension driving the inequality between the gamer class and the rest of modern day society.</h3>
+    <q>A spectre is haunting society - a spectre of gaming [...]</q>
+    <h3>In these desperate times we aim to be a beacon of hope, a safe heaven for gamers like you. By providing news untainted by anti-gamer mindsets and systemic opression or censorship.
+        For we the gamers have to stick together and press on - resist our opressors.</h3>
+
+    <div class="inspiration">Gamers of the world UNITE!</br>All you have to loose are your virginities...</div>
+    <h2 id="data_and_privacy">Data protection and privacy</h1>
+        <h3>This website doesnt use any cookies at all and does not save any user data anywhere. Data transfer is only one way - from the server to the user. That's it. No targeted ads, no trackers, no tracking cookies. Nothing. </h3>
+        <p>I'm waaaaaaaay too lazy to care for the legal stuffs related to EU laws so i will just abstain completely. Not like i have any reason to collect all this data anyway. I aint no data sucking zuckerbergian creature. Your data is safe here. I might use some <i>"strictly functional cookies"</i> here and there in the future.</p>
+        <p>See? The chilling effect of <i>data protection policies</i>? I'm really glad we are in the EU. It's a body big enough that it can just do this and the companies have to cope, cus EU is like the biggest consumer market on earth. I just wish they had a special aid program for gamer class relief</p>
 </div>
+
+<?php require APPROOT . '/views/includes/footer.php'; ?>
--- app/views/misc/contact_index.php
+++ app/views/misc/contact_index.php
@@ -3,10 +3,25 @@ require_once APPROOT . '/views/includes/head.php';
 require_once APPROOT . '/views/includes/nav.php';
 ?>
 
-<div class="sections">
-    <div class="section">
-        <h1>
-            Do you have access to information about an even that in your mind should have been at the spotlight? We are all ears!
-        </h1>
-    </div>
+<head>
+    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/nice_default_tags.css">
+    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/misc.css">
+</head>
+
+<div class="sections nice_default_tags">
+    <div class="inspiration">We, the Gamers, deserve to be heard!</div>
+    <h3>If you have information you believe should see the light of day, contact us! Send us leaks, articles, extracts, stories, events - local, national, global, interplanetary, interstelar - We dont discriminate, we accept it all!</h3>
+    <q>Totalitarianism festers in information darkness or something like that, i dont know im making this shit up on the fly</q>
+    <h2>Where to contact us:</h2>
+    <ul>
+        <li>Email: <i><a href="mailto: amatyniov@gmail.com">amatyniov@gmail.com</a></i> (yes, there is an "a" at the begining)</li>
+        <li>Discord: <i>Matyniov#5840</i></li>
+        <li>Telegram: <i><a href="https://t.me/matyniov" target="_blank">click here</a></i></li>
+    </ul>
+    <h2>Approval process</h2>
+    <p>If you send us an article or anything resembling it, it will undergo a review by our top researchers and journalists. They will evaluate the value, quality and efficacy of your work and if all goes well, it will be posted on our site!</p>
+    <h2>Do you want the source code for the website?</h2>
+    <p>Just contact me i guess</p>
 </div>
+
+<?php require APPROOT . '/views/includes/footer.php'; ?>
